51db3ba48106485b99c80f6e8d4c345be12d4004
code-11 <brendan.ritter@yahoo.com>
2016-09-04 20:15:19 -0400

    Got first iteration done. Simple gravity implemented.

---
+++ assets/gravObject.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravObject : MonoBehaviour {
+
+	public bool immobile;
+	public float gravCutoff;
+	private Rigidbody2D myBody{ get; set;}
+	public static readonly float G = 4; 
+	public Vector2 initialVelocity;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D body=GetComponent<Rigidbody2D> ();
+		if (body != null) {
+			immobile = (body.constraints == RigidbodyConstraints2D.FreezePositionX && body.constraints == RigidbodyConstraints2D.FreezePositionY);
+			myBody = body;
+		} else {
+			Debug.LogError ("gravObject must have a RigidBody2D");
+		}
+
+		myBody.velocity = initialVelocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x= gameObject.transform.position.x;
+		float y = gameObject.transform.position.y;
+		Collider2D[] allInRange= Physics2D.OverlapCircleAll (new Vector2(x,y), gravCutoff);
+		Vector2 totalForce = new Vector2 (0, 0);
+		foreach (Collider2D col in allInRange) {
+			//Ignore yourself. No self acting gravity
+			if (col.gameObject.GetInstanceID () != gameObject.GetInstanceID ()) {
+				gravObject otherGrav = col.gameObject.GetComponent<gravObject> ();
+				Vector2 myPos = gameObject.transform.position;
+				Vector2 otherPos = col.gameObject.transform.position;
+				//TODO: replace with mask at some point
+				if (otherGrav != null) {
+					float otherMass = otherGrav.myBody.mass;
+					float myMass = myBody.mass;
+					Vector2 towards = (otherPos-myPos);
+					float r3 = towards.sqrMagnitude * towards.magnitude;
+					float gravMag = (G * otherMass * myMass / r3);
+					totalForce += gravMag * towards;
+					//Debug.Log (totalForce);
+				}
+			}
+		}
+		myBody.AddForce (totalForce);
+	}
+}
