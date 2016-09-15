b6d0ecf6383e9d04f8d1966a175e92fd36dbc2dd
code-11 <brendan.ritter@yahoo.com>
2016-09-15 19:58:08 -0400

    Started on networking stuff. Ran into an error trying to spawn planets.

---
+++ assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+	private GameObject planetPrefab;
+	void Start(){
+		planetPrefab = Resources.Load ("Prefabs/planet") as GameObject;
+	}
+	void Update()
+	{
+		if (!isLocalPlayer) {
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		transform.Translate(x, y, 0);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdMakePlanet ();
+		}
+	}
+	[Command]
+	public void CmdMakePlanet(){
+		var planet = (GameObject)Instantiate (
+			            planetPrefab,
+			            gameObject.transform.position,
+			            Quaternion.identity);
+		NetworkServer.Spawn (planet);
+	}
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<SpriteRenderer>().material.color = Color.blue;
+	}
+}
