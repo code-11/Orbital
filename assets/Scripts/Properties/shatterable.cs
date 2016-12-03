45c64a425a865e2cecaa63d2405229eb82c1f3db
code-11 <brendan.ritter@yahoo.com>
2016-12-03 11:44:07 -0500

    Shut off shatterable again until I can understand what I'm actually doing.

--- assets/Scripts/Properties/shatterable.cs
+++ assets/Scripts/Properties/shatterable.cs
@@ -24,41 +24,44 @@ public class Shatterable : NetworkBehaviour {
 		}
 
 	}
+		
 	void OnCollisionEnter2D(Collision2D coll) {
-		if (scale>1) {
+		if (enabled) {
+			if (scale > 1) {
 
-			//Collision of two circles is always one point
-			ContactPoint2D firstContact = coll.contacts [0]; 
-			Vector2 pos = firstContact.point;
-			Vector2 normal = firstContact.normal;
-			Vector2 pen1 = (new Vector2 (-normal.y, normal.x)).normalized;
-			Vector2 pen2 = (new Vector2 (normal.y, -normal.x)).normalized;
-			Rigidbody2D otherBody = coll.gameObject.GetComponent<Rigidbody2D> ();
-			GameObject toShatter = null;
-			Vector2 myVel = myBody.velocity;
-			if (otherBody != null) {
-				Vector2 otherVel = otherBody.velocity;
+				//Collision of two circles is always one point
+				ContactPoint2D firstContact = coll.contacts [0]; 
+				Vector2 pos = firstContact.point;
+				Vector2 normal = firstContact.normal;
+				Vector2 pen1 = (new Vector2 (-normal.y, normal.x)).normalized;
+				Vector2 pen2 = (new Vector2 (normal.y, -normal.x)).normalized;
+				Rigidbody2D otherBody = coll.gameObject.GetComponent<Rigidbody2D> ();
+				GameObject toShatter = null;
+				Vector2 myVel = myBody.velocity;
+				if (otherBody != null) {
+					Vector2 otherVel = otherBody.velocity;
 
-				if (myVel.sqrMagnitude > otherVel.sqrMagnitude) {
+					if (myVel.sqrMagnitude > otherVel.sqrMagnitude) {
+						toShatter = gameObject;
+					}
+				} else {
 					toShatter = gameObject;
 				}
-			} else {
-				toShatter = gameObject;
-			}
 
-			//Only one object involved in the collision should evaluate this
-			if (toShatter != null && myVel.sqrMagnitude>velCutoff) {
-				GameObject shard1 = (GameObject)Instantiate (planetPrefab, new Vector2 (pos.x, pos.y)+pen1/2, Quaternion.identity);
-				if (theGravity == null) {
-					Debug.Log ("WAHT");
+				//Only one object involved in the collision should evaluate this
+				if (toShatter != null && myVel.sqrMagnitude > velCutoff) {
+					GameObject shard1 = (GameObject)Instantiate (planetPrefab, new Vector2 (pos.x, pos.y) + pen1 / 2, Quaternion.identity);
+					if (theGravity == null) {
+						Debug.Log ("WAHT");
+					}
+					theGravity.addGrav (shard1.GetComponent<Gravable> ());
+					GameObject shard2 = (GameObject)Instantiate (planetPrefab, new Vector2 (pos.x, pos.y) + pen2 / 2, Quaternion.identity);
+					theGravity.addGrav (shard2.GetComponent<Gravable> ());
+					shard1.GetComponent<Gravable> ().setInitalVelocity (pen1 + myVel);
+					shard2.GetComponent<Gravable> ().setInitalVelocity (pen2 + myVel);
+					theGravity.removeGrav (gameObject.GetComponent<Gravable> ());
+					Destroy (gameObject);
 				}
-				theGravity.addGrav (shard1.GetComponent<Gravable> ());
-				GameObject shard2 = (GameObject)Instantiate (planetPrefab, new Vector2 (pos.x, pos.y)+pen2/2, Quaternion.identity);
-				theGravity.addGrav (shard2.GetComponent<Gravable> ());
-				shard1.GetComponent<Gravable>().setInitalVelocity(pen1+myVel);
-				shard2.GetComponent<Gravable>().setInitalVelocity(pen2+myVel);
-				theGravity.removeGrav (gameObject.GetComponent<Gravable> ());
-				Destroy (gameObject);
 			}
 		}
 	}
