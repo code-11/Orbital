802e86e6a693f84b395e3a34b5901f3f76a4632f
code-11 <brendan.ritter@yahoo.com>
2018-12-09 12:44:28 -0500

    Started on a UI

---
+++ Assets/GUIMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GUIMaster : MonoBehaviour {
+
+	public Camera theCamera;
+	public Text nameLbl;
+
+	public void setName(string name){
+		nameLbl.text=name;
+	}
+
+	public void centerOn(GameObject obj){
+		theCamera.transform.parent = obj.transform; 
+		float zCoord=theCamera.transform.position.z;
+		theCamera.transform.localPosition= new Vector3(0,0,zCoord);
+	}
+}
